6b0710c28558f381201872ab74819180afcbadc9
Callum Kay <greenpear68@yahoo.co.ukcode - helpcode --helpcode --help>
2019-04-05 15:33:22 +0100

    all tests pass

---
+++ controllers/convertHandler.js
@@ -0,0 +1,98 @@
+/*
+*
+*
+*       Complete the handler logic below
+*       
+*       
+*/
+
+function ConvertHandler() {
+  
+  this.getNum = function(input) {
+    
+    let result;
+    let validFraction = /^([1-9]\d*(\.\d+)?)[/](\d+(\.\d+)?)$/g //regex match fractions
+    let validDecimal = /^\d+(\.\d+)?$/ //regex match decimals
+    let split = input.replace(/[^0-9./]/gi, '')
+    
+      if (validDecimal.test(split)) {
+      result = Number(split)
+    } else if (validFraction.test(split)) {
+      let seperate = split.split('/')
+      let fraction = seperate[0] / seperate[1];
+        result = fraction
+    } else {
+      result = 'invalid number'
+    }
+    return result;
+    };
+
+  this.getUnit = function(input) {
+    
+    var result;
+    let unitList = ['gal', 'l', 'kg', 'lbs', 'mi', 'km'];
+    let units = input.toLowerCase()
+    let split = units.replace(/[^a-z]/gi, '')
+    if (unitList.includes(split)) {
+    result = split;
+    } else { 
+    result = 'invalid units';
+    }
+    return result;
+  };
+  
+  this.getReturnUnit = function(initUnit) {
+    const units = {
+      gal: 'l',
+      l: 'gal',
+      mi: 'km',
+      kg: 'lbs',
+      lbs: 'kg',
+      km: 'mi'
+    }
+    return units[initUnit]
+  };
+
+  this.spellOutUnit = function(unit) {
+    const units = {
+      gal: 'gallons',
+      l: 'litres',
+      mi: 'miles',
+      kg: 'kilograms',
+      lbs: 'pounds',
+      km: 'kilometers'
+    }
+        
+    return units[unit];
+  };
+  
+  this.convert = function(initNum, initUnit) {
+    const galToL = 3.78541;
+    const lbsToKg = 0.453592;
+    const miToKm = 1.60934;
+    let result;
+    
+    const convertTable = {
+      gal: galToL,
+      l: 1 / galToL,
+      mi: miToKm,
+      km: 1 / miToKm,
+      lbs: lbsToKg,
+      kg: 1 / lbsToKg
+    };
+  
+    result = Math.round(initNum * convertTable[initUnit] * 10**5) / 10**5;
+
+    return result;
+  };
+  
+  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+    var result;
+  
+    var string = initNum + ' ' + this.spellOutUnit(initUnit) + ' converts to ' + returnNum + ' ' + this.spellOutUnit(returnUnit) + '.'
+    var result = 'initNum: '+ initNum + ', ' + 'initUnit: '+ initUnit + ', ' + 'returnNum: ' + returnNum + ', ' + 'string: ' + string
+    return string;
+  }
+  
+}
+module.exports = ConvertHandler;
---
+++ routes/api.js
@@ -0,0 +1,40 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict';
+
+var expect = require('chai').expect;
+var ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  var convertHandler = new ConvertHandler();
+
+  app.route('/api/convert')
+    .get(function (req, res){
+      var input = req.query.input;
+      var initNum = convertHandler.getNum(input);
+      var initUnit = convertHandler.getUnit(input);
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
+      var string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    console.log(initNum);
+    
+      if (initNum =='invalid number' && initUnit == 'invalid units') {
+        res.json('invalid number and unit')
+      } else if (initNum == 'invalid number') {
+        res.json('invalid number')
+      } else if (initUnit =='invalid units') {
+        res.json('invalid unit')
+      } else {
+        res.json(string)
+        console.log(res.body)
+      }
+    });
+    
+};
